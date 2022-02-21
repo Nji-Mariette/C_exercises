2eab3eaebe40af2ce5c707930bfa156c474510e7
Nji-Mariette <82994508+Nji-Mariette@users.noreply.github.com>
2022-02-21 09:31:11 -0800

    Add files via upload

---
+++ C-Ex1.c
@@ -0,0 +1,15 @@
+/* "compiling and running c file"
+mariettetherese@Njis-MBP ~ % cd Desktop
+mariettetherese@Njis-MBP Desktop % cd assignment
+mariettetherese@Njis-MBP assignment % gcc marie.c -o marie
+mariettetherese@Njis-MBP assignment % 
+
+*/
+
+
+#include<stdio.h>
+int main()
+{
+    printf("\nI am a Boy\n\n");
+    return 0;
+}
